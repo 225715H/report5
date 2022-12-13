f24919985cef3eb243f7c8084c36309bd794ff9c
Motoki Higa <e225715@ie.u-ryukyu.ac.jp>
2022-12-13 21:50:53 +0900

    step2

--- src/Exception.java
+++ src/Exception.java
@@ -1,7 +1,12 @@
 public class Exception {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }   catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました。\n"+ e.getMessage());
+            
+        }
     }
     
 }
